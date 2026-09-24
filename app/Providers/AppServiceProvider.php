4af3464d88c1e9273735494c20bb4f672fff0b11
Nahuel Zamudio <84518078+nahuelz123@users.noreply.github.com>
2026-09-24 11:05:47 -0300

    Usar ejecutor flexible para el bot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Models\User;
+use App\Services\BotActionExecutor;
+use App\Services\FlexibleBotActionExecutor;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -14,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BotActionExecutor::class, FlexibleBotActionExecutor::class);
     }
 
     /**
